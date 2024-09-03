c2d6608762fd123a235c238d749e5bee110def53
Matt Karmazyn <matt@itskarma.wtf>
2024-09-03 04:51:39 -0400

    timer functions

--- pages/index.js
+++ pages/index.js
@@ -1,85 +1,88 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { useEffect, useState } from "react";
+import { useRef } from "react";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Home() {
-  const [desiredRounds, setDesiredRounds] = useState(0);
-  const [roundsRemaining, setRoundsRemaining] = useState(0);
+  const audioRef = useRef(typeof Audio !== "undefined" && new Audio());
+  //TODO: Add a way to change the audio source
+  const [audioSrc, setAudioSrc] = useState("/beep.mp3");
   const [desiredMinutes, setDesiredMinutes] = useState(0);
   const [desiredSeconds, setDesiredSeconds] = useState(5);
   const [desiredRestMinutes, setDesiredRestMinutes] = useState(0);
   const [desiredRestSeconds, setDesiredRestSeconds] = useState(3);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
-  const [restMinutes, setRestMinutes] = useState(0);
-  const [restSeconds, setRestSeconds] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
   const [isResting, setIsResting] = useState(false);
-
-  const stopTimer = () => {
-    setIsRunning(false);
-    setIsResting(false);
-  };
-
-  const resetTimer = () => {
-    setIsRunning(false);
-    setMinutes(desiredMinutes);
-    setSeconds(desiredSeconds);
-  };
 
   const startTimer = () => {
     setIsResting(false);
     setIsRunning(true);
     setMinutes(desiredMinutes);
     setSeconds(desiredSeconds);
-    // runTimer();
   };
 
-  const startRestTimer = () => {
-    setIsResting(true);
+  const stopTimer = () => {
     setIsRunning(false);
-    setRestMinutes(desiredRestMinutes);
-    setRestSeconds(desiredRestSeconds);
+    setIsResting(false);
+    setMinutes(0);
+    setSeconds(0);
   };
 
-  const startNextRound = () => {
-    setRounds(roundsRemaining - 1);
-    startTimer();
-  };
-
-  const runTimer = () => {
+  const toggleRunningResting = () => {
     if (isRunning) {
-      if (minutes === 0 && seconds === 0) {
-        startRestTimer();
-      }
-    }
-    if (isResting) {
-      if (restMinutes === 0 && restSeconds === 0) {
-        if (roundsRemaining > 0) {
-          startNextRound();
-        } else {
-          stopTimer();
-        }
-      }
+      setMinutes(desiredRestMinutes);
+      setSeconds(desiredRestSeconds);
+      setIsRunning(false);
+      setIsResting(true);
+    } else if (isResting) {
+      setMinutes(desiredMinutes);
+      setSeconds(desiredSeconds);
+      setIsRunning(true);
+      setIsResting(false);
     }
   };
 
   useEffect(() => {
+    // add event listener for space bar
+    document.addEventListener("keyup", (event) => {
+      if (event.code === "Space") {
+        if (isRunning || isResting) {
+          stopTimer();
+        } else {
+          startTimer();
+        }
+      }
+    });
+
+    // timer logic: this will run every second
     let interval = setInterval(() => {
       clearInterval(interval);
-      if (seconds === 0) {
-        if (minutes !== 0) {
-          setSeconds(59);
-          setMinutes(minutes - 1);
+      if (isRunning || isResting) {
+        if (seconds === 0) {
+          if (minutes === 0) {
+            // play audio
+            if (audioRef.current) {
+              audioRef.current.src = audioSrc;
+              audioRef.current.play();
+            } else {
+              console.log("Audio not supported");
+            }
+            toggleRunningResting();
+          } else {
+            setSeconds(59);
+            setMinutes(minutes - 1);
+          }
+        } else {
+          setSeconds(seconds - 1);
         }
-      } else {
-        setSeconds(seconds - 1);
       }
     }, 1000);
     return () => clearInterval(interval);
-  }, [seconds, minutes]);
+  }, [seconds, minutes, audioRef, audioSrc]);
 
   return (
     // if the timer is running, set the background color to green, otherwise set it to red
@@ -94,29 +97,37 @@ export default function Home() {
       </Head>
 
       <main>
-        <div className="timer-wrapper">
+        {/* Debug Information */}
+        {/* <div className={styles.debug}>
+          <p>isRunning: {isRunning.toString()}</p>
+          <p>isResting: {isResting.toString()}</p>
+          <p>minutes: {minutes}</p>
+          <p>seconds: {seconds}</p>
+          <p>desiredMinutes: {desiredMinutes}</p>
+          <p>desiredSeconds: {desiredSeconds}</p>
+          <p>desiredRestMinutes: {desiredRestMinutes}</p>
+          <p>desiredRestSeconds: {desiredRestSeconds}</p>
+        </div> */}
+
+        {/* Timer */}
+        <div className={styles.timerWrapper}>
           <h1 className={styles.timer}>
             {minutes < 10 ? `0${minutes}` : minutes}:
             {seconds < 10 ? `0${seconds}` : seconds}
           </h1>
-          <h1 className={styles.timer}>
-            {restMinutes < 10 ? `0${restMinutes}` : restMinutes}:
-            {restSeconds < 10 ? `0${restSeconds}` : restSeconds}
-          </h1>
         </div>
-        <div className="controls">
-          {/* create display for desired number of rounds */}
-          <h2>Rounds: {desiredRounds}</h2>
-          {/* create display for remaining number of rounds */}
-          <h2>Rounds Remaining: {roundsRemaining}</h2>
 
-          {/* create input to set the desired number of rounds */}
-          <input
-            type="number"
-            placeholder="Number of rounds"
-            value={desiredRounds}
-            onChange={(e) => setDesiredRounds(e.target.value)}
-          />
+        <div className={styles.controls}>
+          {/* create display for desired number of minutes */}
+          <h2>Minutes: {desiredMinutes}</h2>
+          {/* create display for desired number of seconds */}
+          <h2>Seconds: {desiredSeconds}</h2>
+          {/* create display for desired number of rest minutes */}
+          <h2>Rest Minutes: {desiredRestMinutes}</h2>
+          {/* create display for desired number of rest seconds */}
+          <h2>Rest Seconds: {desiredRestSeconds}</h2>
+        </div>
+        <div className={styles.controls}>
           {/* create input to set the desired number of minutes */}
           <input
             type="number"
@@ -145,15 +156,19 @@ export default function Home() {
             value={desiredRestSeconds}
             onChange={(e) => setDesiredRestSeconds(e.target.value)}
           />
+        </div>
 
-          {/* create button to start the timer */}
-          <button
-            onClick={startTimer}
-            // disabled={isRunning || isResting}
-            className="start-button"
-          >
-            Start Timer
-          </button>
+        <div className={styles.controls}>
+          {!isRunning && !isResting && (
+            <button onClick={startTimer} className="start-button">
+              Start Timer
+            </button>
+          )}
+          {(isRunning || isResting) && (
+            <button onClick={stopTimer} className="stop-button">
+              Stop Timer
+            </button>
+          )}
         </div>
       </main>
       <Analytics />
